669354e415b2e0b3f71b6018641281a8462c4b37
Leang Chouteau <chouteau.leang@gmail.com>
2025-08-18 16:25:30 +0200

    Initialisation projet P6Buddy

---
+++ src/main/java/com/oc/P6Buddy/P6BuddyApplication.java
@@ -0,0 +1,13 @@
+package com.oc.P6Buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P6BuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P6BuddyApplication.class, args);
+	}
+
+}
