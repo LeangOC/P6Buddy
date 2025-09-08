667dbbda59ed1569c24ce70b41e62fc3204810a2
Leon Veng <veng.leon@gmail.com>
2025-09-08 08:19:09 +0200

    Ajouter tableau pour afficher les relations existantes

--- README.md
+++ README.md
@@ -76,4 +76,5 @@ Implémenter la page "Ajouter relation"
 Mettre en place un Layout global Thymeleaf pour la barre de navigation  
 Pour éviter de répéter le code <nav> et le CSS dans toutes les pages.
 
-
+# solution11bis
+Ajouter tableau pour afficher les relations existantes.
--- src/main/java/com/oc/P6Buddy/controller/RelationController.java
+++ src/main/java/com/oc/P6Buddy/controller/RelationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpSession;
 
+import java.util.List;
+
 @Controller
 public class RelationController {
 
@@ -19,7 +21,15 @@ public class RelationController {
     }
 
     @GetMapping("/relation")
-    public String relationPage() {
+    public String relationPage(HttpSession session, Model model) {
+        User currentUser = (User) session.getAttribute("loggedUser");
+        if (currentUser == null) {
+            return "redirect:/login"; // sécurité si pas connecté
+        }
+
+        // Charger directement la liste des relations existantes
+        model.addAttribute("relations", relationService.getBuddyEmails(currentUser));
+
         return "relation"; // templates/relation.html
     }
 
@@ -33,6 +43,10 @@ public class RelationController {
         }
         String message = relationService.addRelation(currentUser, email);
         model.addAttribute("message", message);
+
+        //Recharger la liste des relations après ajout
+        model.addAttribute("relations", relationService.getBuddyEmails(currentUser));
+
         return "relation";
     }
 }
